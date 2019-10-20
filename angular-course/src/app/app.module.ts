857184130c29f53fb6b42336849fd9145a98f34d
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-10-20 10:23:33 +0300

    two-way data-binding. the input and output are in sync

--- angular-course/src/app/app.module.ts
+++ angular-course/src/app/app.module.ts
@@ -4,7 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component'
+import { ServersComponent } from './servers/servers.component';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { ServersComponent } from './servers/servers.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
